0388dfeef7dab8a4f5a54536245f5cc05f37eabf
sajjad <sajjad.darvish.yektayi@gmail.com>
2023-10-23 09:31:16 +0330

    first commit

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-green-600">
+        <span>+</span>
+        <span>Click me</span>
+      </Button>
+    </div>
+  );
+}
