f0f84bab7608ac9cdd35c2254b0c1b4163e5e46f
sajjad <sajjad.darvish.yektayi@gmail.com>
2023-10-24 13:35:33 +0330

    Update documents page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,6 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
-    <div>DocumentsPage</div>
-  )
-}
-export default DocumentsPage
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden w-auto h-auto"
+        priority
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block w-auto h-auto"
+        priority
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
+};
+export default DocumentsPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={inter.className}>
+      <body className={inter.className + " h-screen"}>
         <ConvexClientProvider>
           <ThemeProvider
             attribute="class"
